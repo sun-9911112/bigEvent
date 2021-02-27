8e14dbcf2dfba7f4f8cf94f809e1b93b52d159e5
孙钰欣帅 <sun2396187213@163.com>
2021-02-27 11:30:38 +0800

    验证密码

--- login/login.js
+++ login/login.js
@@ -1,4 +1,6 @@
 let layer = layui.layer;
+let form = layui.form;
+
 
 // ------------------------------------------切换
 // 去注册
@@ -12,15 +14,60 @@ $("#goto-login").on("click", function() {
   $("#register").hide();
 });
 
+
+
+
+
+// -----------------------------------------验证
+// 需求：
+//    1. 用户名、密码、重复密码不能为空  
+//    2. 密码、重复密码   长度 6~12 位，且不能出现空格：   非空格类字符；\S
+//    3. 密码 和 重复密码 必须一致 
+
+
+form.verify({
+  // 长度 6~12 位，且不能出现空格
+  changdu: function(value, dom) {
+    let reg = /^\S{6,12}$/;
+    if (reg.test(value) == false) {
+      return "密码必须6到12位，且不能出现空格";
+    }
+  },
+
+  // 密码 和 重复密码 必须一致   same设置给谁？设置重复密码
+  same: function(value, dom) {
+    // value：谁的value？重复密码的值
+    // 验证：获取密码值！密码的值 如何获取?  设置类名.val(); 
+    //       可以不写正则！
+    if ($(".pwd").val() != value) {
+      return "密码和重复密码必须一致!"
+    }
+  },
+
+
+
+
+  // 简写：数组[正则、不满足时文字提醒]
+  // pass: [
+  //   /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+  // ]
+});
+
+
+
+
+
+
+
 // -------------------------------------------注册
 // html：form表单！按钮/表单元素 name属性和参数名一致！
 // JS ：收集数据！$.ajax()
-
 $("#register form").on("submit", function(e) {
   e.preventDefault();
 
   // 1.收集数据  JQ  layui.form模块
   let data = $(this).serialize();
+
 
   // 2.提交数据
   $.ajax({
@@ -37,6 +84,11 @@ $("#register form").on("submit", function(e) {
         // 注册表单重置！
         $("#register form")[0].reset();
       }
+
+
     }
   })
-})
+
+
+
+});
